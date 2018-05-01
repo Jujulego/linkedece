ae4c2c69b226adfca045cc2bac260bd5bda8aa0d
Julien CAPELLARI <julien@capellari.net>
2018-05-01 10:29:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 01/05/2018
+ * Time: 10:32
+ */
+?>
+<html>
+    <head>
+        <meta charset="utf8">
+        <title>LinkedECE</title>
+    </head>
+    <body>
+        <h1>A vous de jouer !</h1>
+    </body>
+</html>
