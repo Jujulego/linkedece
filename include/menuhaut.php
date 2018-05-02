0c91ba479553eaa15426cced2bd7233f9f882d68
Julien CAPELLARI <julien@capellari.net>
2018-05-02 12:34:58 +0200

    Coooolll !

---
+++ include/menuhaut.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nolwenn
+ * Date: 02/05/2018
+ * Time: 11:34
+ */
+?>
+<div id="menu_haut">
+    <a href="../accueil.php">Accueil</a>
+
+    <div id="espace"></div>
+
+    <a href="Emplois.php"> Emplois </a>
+    <a href="Notifications.php"> Notifications </a>
+    <a href="Réseau.php"> Réseau </a>
+    <a href="Profil.php"> Profil </a>
+</div>
