44324657b5e42d95370a9a1e150ce20775af4c34
Nolwenn <nolwenn.gentric@gmail.com>
2018-05-02 12:46:21 +0200

    ajout connexion

---
+++ connexion.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nolwenn
+ * Date: 02/05/2018
+ * Time: 12:45
+ */
+?>
+<!DOCTYPE html>
+<html>
+
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Connexion</title>
+
+</head>
+<body>
+
+<div id=conteneur>
+    <div class= co><input type="button" value="Se connecter"></div>
+    <div class= co><SELECT name="titre" size="1">
+            <OPTION>Etudiant
+            <OPTION>Professeur
+            <OPTION>Partenaire
+        </SELECT></div>
+
+
+</div>
+
+
+<div class= co1 >Créer un compte : </div>
+<a href="index.html"> Mot de passe oublié? </a>
+<div id=conteneur2><div class=rond><br>&#10004</div></div>
+
+
+</body>
+</html>
