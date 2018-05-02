02902c9c86e1d33132008ef2cd87a22a25f2373b
Nolwenn <nolwenn.gentric@gmail.com>
2018-05-02 11:55:01 +0200

    Début implémentation visuel

---
+++ MenuHaut.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nolwenn
+ * Date: 02/05/2018
+ * Time: 11:34
+ */
+?>
+<div id=conteneur>
+		<a href="Accueil.html"> Accueil </a>
+
+		<div id="espace"></div>
+
+		<a href="Emplois.html"> Emplois </a>
+		<a href="Notifications.html"> Notifications </a>
+		<a href="Réseau.html"> Réseau </a>
+		<a href="Profil.html"> Profil </a>
+	</div>
