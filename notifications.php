7ac8eff9028842bfc1f418ac5fc03ffb20ac1e19
PC_DAMIEN\Damien <damien92g@gmail.com>
2018-05-03 04:44:19 +0200

    ajout de l'image notification

---
+++ notifications.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Damien
+ * Date: 03/05/2018
+ * Time: 04:35
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <link rel="stylesheet" href="css/style_accueil.css" />
+    <link rel="stylesheet" href="css/style_general.css" />
+    <link rel="stylesheet" href="css/style_menuhaut.css" />
+
+    <title>Accueil</title>
+</head>
+
+<body>
+<?php include("include/menuhaut.php") ?>
+
+<div id="conteneur">
+    <section id="profil">
+        <img src="profil.png" width="100px" height="100px" alt="Photo de profil par défault" />
+        <p>Prénom Nom</p>
+        <p>Titre</p>
+        <p>Email</p>
+        <p>50 relations</p>
+    </section>
+
+    <section id="mur">
+        <article>
+            <p>
+            <img src="notif.png" width="60px" height="60px" alt="Photo de notification" />
+                Contenu de la notification
+            </p>
+            <p>nom_notif</p>
+        </article>
+
+    </section>
+</div>
+</body>
+</html>
