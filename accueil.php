111584b1b1491510ba643d166f232da752e56ccf
Nolwenn <nolwenn.gentric@gmail.com>
2018-05-03 11:52:45 +0200

    rangement image et ajout lieu et sentiment

--- accueil.php
+++ accueil.php
@@ -45,38 +45,57 @@ if (!isset($_SESSION["pseudo"])) {
 
                         <textarea  style="width: 300px;height:100px" >Votre statut...</textarea>
                         <div id="butstatut">
-                            <div class="butstatut1"> <a href="http://ton lien"><img src="ajoutimage.png".png" width="40px" height="38px" alt= "ajout images"></a></div>
-                            <div class="butstatut1"> <a href="http://ton lien"><img src="ajoutvideo.png" width="35px" height="38px" alt= "ajout vidéos"></a></div>
+                            <div class="butstatut1"> <a href="http://ton lien"><img src="images/ajoutimage.png".png" width="40px" height="38px" alt= "ajout images"></a></div>
+                            <div class="butstatut1"> <a href="http://ton lien"><img src="images/ajoutvideo.png" width="35px" height="38px" alt= "ajout vidéos"></a></div>
 
                         </div>
 
                     </div>
+                    <table>
+                        <tr>
+                            <td>
+                                <a href="http://ton lien"><img src="images/logolieu.png" width="30px" height="30px" alt= "lieu"/></a>
+                            </td>
+                            <td>
+                                <input type="text" name="lieu" size="10" />
+
+                            </td>
+
+                            <td>
+                                <label>Ressenti: </label>
+                            </td>
+                            <td>
+                                <input type="text" name="lieu" size="10" />
+                            </td>
+                        </tr>
+                    </table>
                     <hr>
                     <div class="butstatut2">
                     <SELECT name="confidentialité" size="1">
                         <OPTION>Public
                         <OPTION>Relation
                     </SELECT>
-                        <input type="button" float="right" width="10px" height="10px" value="publier" >
+                        <input type="button" float="right" width="10px" height="10px" value="Publier" >
                     </div>
                 </article>
 
                 <article>
                     <div id="post">
-                        <img src="profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                        <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
                         <p>Prenom Nom</p>
                         <div class="posteact">
                             <p>Poste actuel</p>
                         </div>
                         <div class="lien">
-                        <a href="http://ton lien"><img src="aime.png" width="30px" height="30px" alt= "pouce j'aime"></a>
-                        <a href="http://ton lien"><img src="commentaire.png"  width="30px" height="30px" alt= "commentaire"></a>
+                        <a href="http://ton lien"><img src="images/aime.png" width="30px" height="30px" alt= "pouce j'aime"></a>
+                        <a href="http://ton lien"><img src="images/commentaire.png"  width="30px" height="30px" alt= "commentaire"></a>
                         </div>
                         <div class ="lienpartage">
                             <a href="http://ton lien">
-                                <img src="partagebleu.png"  width="30px" height="30px" alt= "partage">
+                                <img src="images/partagebleu.png"  width="30px" height="30px" alt= "partage">
                             </a>
                         </div>
+
                      </div>
 
                 </article>
