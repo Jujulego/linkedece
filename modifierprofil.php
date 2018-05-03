ada63b2d0321c55e97f8f83fdddccc58138412da
PC_DAMIEN\Damien <damien92g@gmail.com>
2018-05-03 19:22:21 +0200

    fin du formulaire de modification de profil

--- modifierprofil.php
+++ modifierprofil.php
@@ -23,31 +23,38 @@
 
 <div id ="menugauche">
     <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défaut" href=""/>
-</div>
 
-<div>
-    <form method="post">
+    <form enctype="multipart/form-data" method="post">
+        <br>
+
+
+
+        <input type="file" accept="image/*" name="imgfile" /><br>
+        <input type="submit" value="Valider"/>
+        <br>
+        <br>
+
         <table>
             <tr>
-                <td><label for="prenom">Prénom</label></td>
+                <td><label for="prenom">Prénom :</label></td>
                 <td><input id="prenom" type="text" name="prenom"/></td>
-                <td><label for="nom">Nom</label></td>
+                <td><label for="nom">Nom :</label></td>
                 <td><input id="nom" type="text" name="nom"/></td>
             </tr>
             <tr>
-                <td><label for="titre">Titre</label></td>
+                <td><label for="titre">Titre :</label></td>
                 <td><input id="titre" type="text" name="titre"/></td>
-                <td><label for="poste">Poste actuel</label></td>
+                <td><label for="poste">Poste actuel :</label></td>
                 <td><input id="poste" type="text" name="poste"/></td>
             </tr>
             <tr>
-                <td><label for="pays">Pays</label></td>
+                <td><label for="pays">Pays :</label></td>
                 <td><input id="pays" type="text" name="pays"/></td>
-                <td><label for="ville">Ville</label></td>
+                <td><label for="ville">Ville :</label></td>
                 <td><input id="ville" type="text" name="ville"/></td>
             </tr>
             <tr>
-                <td><label for="secteur">Secteur</label></td>
+                <td><label for="secteur">Secteur :</label></td>
                 <td><input id="secteur" type="text" name="secteur"/></td>
             </tr>
             <tr>
@@ -62,7 +69,7 @@
                 </td>
                 <td></td>
                 <td></td>
-                <td><input type="button" value="Enregistrer les modifications" ></td>
+                <td><input type="submit" value="Enregistrer les modifications" ></td>
             </tr>
 
 
