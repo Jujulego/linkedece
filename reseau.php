b95297958b5000ed2e40c4f73e0cd99fa61c9bb6
PC-COURS\Damien <damien92g@gmail.com>
2018-05-04 17:51:44 +0200

    ajout des pages reseau et ajout_reseau

--- reseau.php
+++ reseau.php
@@ -12,7 +12,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    <link rel="stylesheet" href="css/style_accueil.css" />
+    <link rel="stylesheet" href="css/style_reseau.css" />
     <link rel="stylesheet" href="css/style_general.css" />
     <link rel="stylesheet" href="css/style_menuhaut.css" />
 
@@ -31,31 +31,81 @@
         <p>50 relations</p>
     </section>
 
-    <section id="mur">
-        <article>
-            <div id="post">
+    <div id="supermur">
+        <p id="btnajout">
+            <a href="ajoutreseau.php">Ajouter des relations</a>
+        </p>
+        <section id="murreseau">
+            <article class="liencontact">
                 <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
-                <p>Prenom Nom</p>
-
-                <textarea  style="width: 300px;height:100px" >Votre statut...</textarea>
-            </div>
-        </article>
-        <article>
-            <div id="post">
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
                 <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
-                <p>Prenom Nom</p>
-                <div class="posteact"><p>Poste actuel</p></div>
-            </div>
-            <a href="http://ton lien"><img src="images/pouce j'aime.png" width="30px" height="30px" alt= "pouce j'aime"></a>
-            <a href="http://ton lien"><img src="images/commentaire.png" width="30px" height="30px" alt= "commentaire"></a>
-            <a href="http://ton lien"><img src="images/partagebleu.png" width="30px" height="30px" alt= "commentaire"></a>
-        </article>
-
-        <article>
-            <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
-            <p>Prenom Nom</p>
-        </article>
-    </section>
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+            <article class="liencontact">
+                <img src="images/profil.png" width="60px" height="60px" alt="Photo de profil par défault" />
+                <a href="#page de profil du contact">Prenom Nom</a>
+            </article>
+        </section>
+    </div>
 </div>
 </body>
 </html>
